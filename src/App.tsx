ddd0f3100dae8112885569f41607b845b89dd736
dnaz <zibranaby069@gmail.com>
2025-09-21 08:07:30 +0700

    frist commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+function App() {
+  return (
+  <div className="min h-screen bg-black/75">
+    <Routes>
+    <Route path='/' />
+  </Routes>
+  </div> 
+  )
+}
+
+export default App
