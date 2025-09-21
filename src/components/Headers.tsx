ebc33d69bcc71d1ebbe3fb15c20959951f26fb68
dnaz <zibranaby069@gmail.com>
2025-09-21 09:25:00 +0700

    feat(Header) styyling the headers

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
-
+import { useState } from 'react';
+import { Menu, X } from 'lucide-react';
 export default function Headers() {
+  const [open, setOpen] = useState(false);
+  const onClicked = () => {
+    setOpen(!open)
+  }
   return (
-    <div>Headers</div>
+    <div className="text-white shadow-xl p-6 flex flex-row justify-between">
+      <div className="font-bold">FArel&jbran</div>
+      <button
+        onClick={onClicked}
+      > {open ? <X size={28} /> : <Menu size={28} />}
+      </button>
+      {open && (<div className='absolute bg-gray-500/90 top-0 right-0 w-72 h-screen shadow-gray-700 shadow-lg'>
+        <div className="w-full flex flex-row justify-end p-6">
+          <button
+            onClick={onClicked}
+          > {open ? <X size={28} /> : <Menu size={28} />}
+          </button>
+
+        </div>
+      </div>)}
+    </div>
   )
 }
