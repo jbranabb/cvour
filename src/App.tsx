eaca9686082b4a52b4c4d545e9ca6600aee8a1df
dnaz <zibranaby069@gmail.com>
2025-09-21 09:25:00 +0700

    feat(app) addiing headers in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Headers from './components/Headers'
 function App() {
   return (
   <div className="min h-screen bg-black/75">
+    <Headers></Headers>
     <Routes>
     <Route path='/' />
   </Routes>
