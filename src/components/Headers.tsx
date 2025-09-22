923a8d90cdfcddb3801fa747c9516a8f5043ad34
dnaz <zibranaby069@gmail.com>
2025-09-22 16:08:20 +0700

    feat(Headers) make ui more beautiful for headers

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -11,21 +11,21 @@ export default function Headers() {
       <div className="font-bold">FArel&jbran</div>
       <button
         onClick={onClicked}
-      > {!open && <Menu size={28} /> }
+      > {open ? <Menu size={28} /> :  <Menu size={28} /> }
       </button>
         <div className={`fixed inset-0 w-screen h-screen bg-black/30 duration-200
           transition-opacity ease-in-out
-        ${open ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} flex justify-end z-10`}
+        ${open ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} flex justify-end`}
          onClick={onClicked}
          >
-          <div className={`bg-white/15  w-64 font-bold h-full p-6 backdrop-blur-sm transform transition-transform duration-300
+          <div className={`bg-white/10 w-64 font-bold h-full p-6 backdrop-blur-sm transform transition-transform duration-300
           ease-in-out ${open ? "translate-x-0" : "translate-x-full"}
           `} onClick={(e) => e.stopPropagation()} >
             <div className="flex justify-end">
             <X size={28} onClick={onClicked} />  
             </div>
             <div className="flex flex-col gap-5
-            text-xl text-black/55">
+            text-xl text-black/30">
             <Link to="" className=' transform transition-transform hover:text-white hover:scale-95'>Home</Link>
             <Link to="" className=' transform transition-transform hover:text-white hover:scale-95'>About Us</Link>
             <Link to="" className=' transform transition-transform hover:text-white hover:scale-95'>Our Projects</Link>
