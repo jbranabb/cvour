17ebed97a5b98c28b88c01da8db185450281c991
dnaz <zibranaby069@gmail.com>
2025-09-23 12:05:06 +0700

    refactor(App) refactoring from using react-router to react-scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
 import Headers from './components/Headers'
 import Home from './components/Home'
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import {Element}  from "react-scroll";
+
 import { BackgroundBeams } from './components/ui/background-beams'
 function App() {
   return (
     <div className="min-h-screen bg-black overflow-y-hidden w-full ">
     <BackgroundBeams className='absolute inset-0 pointer-events-none z-0 ' />
+    <Element name='home'>
     <Headers></Headers>
-    <Routes>
-    <Route path='/' element={ <Home/> } />
-    <Route path='/aboutus' element={ <Home/> } />
-  </Routes>
+    <Home />
+    </Element> 
+    <Element name='about'>
+    <About />
+    </Element> 
+    <Element name='projects'>
+    <Projects />
+    </Element> 
+    <Element name='contact'>
+    <Contact />
+    </Element> 
   </div> 
   )
 }
