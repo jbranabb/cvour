aa0598151640ead2bf66b4ab40f2a47e810cc1e2
dnaz <zibranaby069@gmail.com>
2025-09-30 22:04:19 +0700

    feat(about) improving about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,49 +1,30 @@
-import React from 'react'
-import { SiLaravel, SiAndroid } from 'react-icons/si'
-import { FiSettings } from 'react-icons/fi'
 import { FlipWords, } from './ui/flipwords'
+import { AiFillRocket } from 'react-icons/ai'
+import { MdPhoneIphone, MdWeb } from 'react-icons/md'
+import { RiMoneyDollarCircleFill } from 'react-icons/ri'
+import { TbSettingsAutomation } from 'react-icons/tb'
+import { HiUsers } from 'react-icons/hi'
 export default function About() {
     return (
         <section className='w-screen min-h-screen text-white font-bold'>
-            <div className='flex flex-col p-10'>
+            <div className='flex flex-col p-10 h-screen '>
                 <div className="flex flex-row gap-2 text-3xl font-medium">
                     Build
                     <FlipWords
                         className='p-0 m-0 font-bold'
                         words={['Beautiful', 'Modern', 'Cool',]} />
-                        </div>
-                <h1 className='text-3xl font-medium'>Apps With Us</h1>
-                <h1 className='text-inherit'>Empowering Small Businesses Through Technology”</h1>
-                <p className="font-medium text-xs">We believe every business, no matter how small, has the potential to grow. Through the apps and websites we build, we help small companies upgrade their tools, making them more efficient, modern, and ready to compete in the digital era.</p>
-                <div className='mt-3 text-xl'>What we offer..</div>
-                <div className="w-full rounded grid gap-4 pt-2 ">
-                    {/* It COns */}
-                    <div className="bg-gray-500/15 p-2 rounded-2xl flex flex-row gap-2 items-center">
-                        <FiSettings size={40} className='hover:rotate-120 hover:scale-110 ease-in-out transition-transform'/>
-                        <div className="flex flex-col">
-                            <h1>IT Conslutant</h1>
-                            <h1 className='font-medium text-xs'>consulting how do'd like to do with your apps</h1>
-                        </div>
-                    </div>
-                    {/* web Devp */}
-                    <div className="bg-gray-500/15 p-2 rounded-2xl flex flex-row gap-2 items-center">
-                        <SiLaravel size={40} className='hover:animate-pulse hover:scale-110 ease-in-out transition-transform'/>
-                        <div className="flex flex-col">
-
-                            <h1>Website Development</h1>
-                            <h1 className='font-medium text-xs'>Lorem ipsum, dolor sit amet consectetur adipisicing </h1>
-                        </div>
-                    </div>
-                    {/* web Devp */}
-                    <div className="bg-gray-500/15 p-2 rounded-2xl flex flex-row gap-2 items-center">
-                        <SiAndroid size={40} className='hover:scale-110 hover:animate-pulse ease-in-out transition-transform'/>
-                        <div className="flex flex-col">
-                            <h1>Mobile Development</h1>
-                            <h1 className='font-medium text-xs'>consulting how do'd like to do with your apps</h1>
-                        </div>
-                    </div>
-
                 </div>
+                <h1 className='text-3xl font-medium'>Apps With Us</h1>
+                <h1 className='text-inherit w-[80%]'>Empowering Small Businesses Through Technology”</h1>
+                <p className="font-medium text-xs py-2">We believe every business, no matter how small, has the potential to grow. Through the apps and websites we build, we help small companies upgrade their tools, making them more efficient, modern, and ready to compete in the digital era.</p>
+                <div className='text-xl'>What we offer..</div>
+                <ul className="mt-3 space-y-2 text-sm font-medium text-gray-300">
+                    <li className='flex gap-2 items-center'> <TbSettingsAutomation size={30} /> Fully customizable solutions tailored to your business needs</li>
+                    <li className='flex gap-2 items-center'> <AiFillRocket size={30} /> Cross-platform apps (Android, and Web) in one development cycle</li>
+                    <li className='flex gap-2 items-center'><MdWeb size={30} /> Scalable & future-proof technology that grows with your business</li>
+                    <li className='flex gap-2 items-center'><RiMoneyDollarCircleFill size={30} /> Affordable and transparent pricing with no hidden costs</li>
+                    <li className='flex gap-2 items-center'><HiUsers size={30} /> A partner mindset — we grow when your business grows</li>
+                </ul>
             </div>
         </section>
     )
