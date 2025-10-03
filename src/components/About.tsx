05450996eefb1e0b8eafb5e59e408c1857244c46
dnaz <zibranaby069@gmail.com>
2025-10-03 16:50:00 +0700

    refactor(About) adding some web development and android development

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,16 +3,14 @@ import { AiFillRocket } from 'react-icons/ai'
 import { MdPhoneIphone, MdWeb } from 'react-icons/md'
 import { RiMoneyDollarCircleFill } from 'react-icons/ri'
 import { TbSettingsAutomation } from 'react-icons/tb'
-import { HiUsers } from 'react-icons/hi'
+import { HiUsers, HiSparkles } from 'react-icons/hi'
+import { SiAndroid} from 'react-icons/si'
 export default function About() {
     return (
         <section className='w-screen min-h-screen text-white font-bold'>
             <div className='flex flex-col p-10 h-screen '>
                 <div>
                 </div>
-               <img 
-               className='mb-6 h-60 object-cover rounded-sm '
-               src="src/assets/mnt.jpeg" alt="" />
                 <div className="flex flex-row gap-2 text-3xl font-medium">
                     Build
                     <FlipWords
@@ -26,10 +24,36 @@ export default function About() {
                 <ul className="mt-3 space-y-2 text-sm font-medium text-gray-300">
                     <li className='flex gap-2 items-center'> <TbSettingsAutomation size={30} /> Fully customizable solutions tailored to your business needs</li>
                     <li className='flex gap-2 items-center'> <AiFillRocket size={30} /> Cross-platform apps (Android, and Web) in one development cycle</li>
-                    <li className='flex gap-2 items-center'><MdWeb size={30} /> Scalable & future-proof technology that grows with your business</li>
+                    <li className='flex gap-2 items-center'><MdPhoneIphone size={30} /> Scalable & future-proof technology that grows with your business</li>
                     <li className='flex gap-2 items-center'><RiMoneyDollarCircleFill size={30} /> Affordable and transparent pricing with no hidden costs</li>
                     <li className='flex gap-2 items-center'><HiUsers size={30} /> A partner mindset — we grow when your business grows</li>
                 </ul>
+                <div className="flex flex-col pt-8 gap-4">
+                    <div className="w-[100%] rounded-2xl h-20 bg-white/15 border-1 border-white/25 
+                    flex flex-row gap-4 items-center p-6 hover:bg-white/20 transition-all hover:border-white/25">
+                      <SiAndroid size={80} />
+                      <div className="flex flex-col">
+                      <h1 className="">Android Development</h1>
+                      <p className="font-light text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, possimus?</p>
+                      </div>
+                    </div>
+                    <div className="w-[100%] rounded-2xl h-20 bg-white/15 border-1 border-white/25 
+                    flex flex-row gap-4 items-center p-6 hover:bg-white/20 transition-all hover:border-white/25">
+                      <MdWeb size={80} />
+                      <div className="flex flex-col">
+                      <h1 className="">Web Development</h1>
+                      <p className="font-light text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, possimus?</p>
+                      </div>
+                    </div>
+                    <div className="w-[100%] rounded-2xl h-20 bg-white/15 border-1 border-white/25 
+                    flex flex-row gap-4 items-center p-6 hover:bg-white/20 transition-all hover:border-white/25">
+                      <HiSparkles size={80} />
+                      <div className="flex flex-col">
+                      <h1 className="">Ai Development</h1>
+                      <p className="font-light text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, possimus?</p>
+                      </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
