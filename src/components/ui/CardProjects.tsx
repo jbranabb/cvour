a3cacbc066d221afc1068bf170d85a3667ed3d45
dnaz <zibranaby069@gmail.com>
2025-10-05 04:30:00 +0700

    feat(CardsProjects) adding breakpoints styling in md

--- src/components/ui/CardProjects.tsx
+++ src/components/ui/CardProjects.tsx
@@ -32,9 +32,11 @@ export default function CardProjects( cards : Cards) {
             </div>
             <div className="px-8 pb-4 flex flex-col gap-4 sm:gap-2 ">
                 <h1 className="font-bold sm:text-lg ">{cards.title}</h1>
-                <div className="flex flex-row gap-1 text-xs items-center sm:scale-120 sm:pl-10"> <MdLocationPin size={12} className='sm:scale-120'/>{cards.location}</div>
-                <p className='text-xs font-medium sm:text-base'>{cards.subTitle}</p> 
-                <div className='flex flex-row sm:scale-110 sm:pl-4'>
+                <div className="flex flex-row gap-1 text-xs items-center
+                 sm:scale-120 sm:pl-10 md:pl-6"> <MdLocationPin size={12} className='sm:scale-120'/>{cards.location}</div>
+                <p className='text-xs font-medium sm:text-base md:text-sm'>{cards.subTitle}</p> 
+                <div className='flex flex-row sm:scale-110 
+                sm:pl-4 md:pl-2 '>
                 {cards.tools.map((tool, i)=> (<div key={i}>{tool}</div>))}
                 </div>
             </div>
