e9a329241b38679e5aad3bb4cff4f2d81038e680
dnaz <zibranaby069@gmail.com>
2025-10-06 04:50:00 +0700

    feat(Home) hidden socialmedia icons when md

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,11 +7,12 @@ export default function Home() {
       ">
         <div className="text-2xl sm:text-4xl md:text-6xl">Servio</div>
         <div className="text-4xl sm:text-6xl md:text-7xl lg:text-center lg:w-[90%]">You Know The business, we Know the Chemistry</div>
-        <div className="font-light text-gray-300 sm:text-xl md:text-2xl lg:text-center lg:w-[70%]">Every business has its own rhythm. That’s why we’re 
+        <div className="font-light text-gray-300 sm:text-xl md:text-2xl lg:text-center lg:w-[70%] lg:mt-2 ">Every business has its own rhythm. That’s why we’re 
           here not just as a service provider, but as a partner who understands your flow—so the
            chemistry between strategy and execution works in harmony.</div>
         <div className='flex- flex-row'>
           <div className="flex flex-row gap-2 pt-2 ">
+            <div className="socialMediaLogo flex gap-2 blcok lg:hidden ">
             {/* Github */}
             <div className='hover:bg-white rounded-full duration-150'>
               <SiGithub size={40} className='hover:text-blue-950 hover:scale-110 transition-all' />
@@ -24,6 +25,8 @@ export default function Home() {
             <div className='hover:bg-gradient-to-bl from-purple-700 to-orange-400 rounded-xl duration-150'>
               <SiInstagram size={40} className='hover:text-white hover:scale-80 transition-all' />
             </div>
+            </div>
+
             <div className="w-28 h-10 bg-white/5 flex justify-center items-center transition-shadow
             duration-500 ease-in-out
             md:w-40 
