83ca1a336f39025de95fe379a03c2db35f97b08f
dnaz <zibranaby069@gmail.com>
2025-10-06 04:35:00 +0700

    feat(Contact) addding some breakpoints when in md

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,15 +6,17 @@ export default function Contact() {
   return (
     <section className='w-screen min-h-screen pt-4'>
       <div className="w-full min-h-screen text-white">
-        <div className="px-2 py-2">
+        <div className="md:flex md:flex-row justify-center items-start">
+        <div className="marquee md:w-[50%]">
+           <div className="px-4 py-2">
           <h1 className="font-bold text-2xl sm:text-4xl">Why They Choose Us?</h1>
           <h1 className="font-medium pb-2 w-[70%] sm:text-lg">Trusted by businesses and loved by users across industries.</h1>
         </div>
-        <div className=" bg-amber-400 h-[20%]"></div>
         <Marquee />
         <MarqueeRight />
-        <div className="">
-          <div className="pt-10 pl-2 font-bold text-3xl sm:text-4xl">Get In Touch</div>
+        </div>
+        <div className="formContact md:w-[50%]">
+          <div className="pt-10 md:pt-0 pl-2 font-bold text-3xl sm:text-4xl md:pt-0l">Get In Touch</div>
           <div className="pl-3 font-medium sm:text-lg">Lets Work Together</div>
           <TextFileds title="Name" name='username' placeholder="Frist Name" />
           <TextFileds title="Email addres" name='email' placeholder="letsworktogher@example.com" />
@@ -28,17 +30,19 @@ export default function Contact() {
               font-medium
               w-[90%] min-h-32"></textarea>
           </div>
-          <div className="w-screen flex justify-center pt-6">
+          <div className="md:w-[100%] flex ">
+          <div className="w-screen flex justify-center pt-6 md:justify-center md:px-4">
             <button type="submit"
               className="bg-white text-black rounded-4xl font-bold
            px-8 py-2 shadow-white shadow-md transition-all
            hover:scale-102
            ease-in-out duration-500 hover:shadow-2xl hover:shadow-white/80">Send Message</button>
           </div>
+          </div>
+        </div>
         </div>
         <Footer />
       </div>
     </section>
-
   )
 }
