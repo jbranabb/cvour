515d0e685f51ad7b356611fbc41082cd4e598b85
dnaz <zibranaby069@gmail.com>
2025-10-10 11:47:31 +0700

    refactor(Projects) refactoring from using dummy data to real data case

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,65 +2,67 @@ import React from 'react'
 import CardProjects from './ui/CardProjects'
 import ToolsProjects from './ui/ToolsProjects'
 import { SiFlutter, SiSupabase, SiFirebase } from 'react-icons/si'
+import { createClient } from '@supabase/supabase-js'
+import { useState, useEffect } from 'react'
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
+type ParamsProjects = {
+  id: number,
+  tittle: string,
+  subtite: string,
+  img_path: string,
+  type: string,
+  location: string,
+  created_at: string;
+  tools: string[];
+}
 export default function Projects() {
-   const projectTools = [
-    { title: 'Flutter', icons: <SiFlutter size={10} />,},
-    { title: 'Supabase', icons: <SiSupabase size={10} />,},
-    { title: 'Firebase', icons: <SiFlutter size={10} />},
-    { title: 'OneSignal', icons: <SiFirebase size={10} />,},
+  const [projects, setProjects] = useState<ParamsProjects[]>([])
+  async function getProjects() {
+    const { data, error } = await supabase.from('servio_projects').select('*');
+    console.log('📦 Data dari Supabase:', data)
+    console.log('❌ Error dari Supabase:', error)
+    if (error) console.error('Error fetching the data', error)
+    else setProjects(data);
+  }
+  useEffect(() => {
+    console.log('fetching....')
+    getProjects();
+  }, []);
+
+  const projectTools = [
+    { title: 'Flutter', icons: <SiFlutter size={10} />, },
+    { title: 'Supabase', icons: <SiSupabase size={10} />, },
+    { title: 'Firebase', icons: <SiFlutter size={10} /> },
+    { title: 'OneSignal', icons: <SiFirebase size={10} />, },
   ];
+  console.log(projects.map((hei) => {
+    console.log(hei.tittle)
+  }))
   return (
     <section className='w-screen min-h-screen xl:min-h-0 xl:mb-60 p-4 mt-25 md:mt-30 lg:mt-0 '>
       <div className="gap-10 sm:px-10">
         <div className="">
-        <h1 className="text-white text-2xl font-extrabold sm:text-3xl md:text-5xl">Stuff We’ve Been Cooking</h1>
-        <p className="font-medium text-white text-sm sm:text-lg md:text-2xl">Check out some of the cool projects we’ve dropped.</p>
+          <h1 className="text-white text-2xl font-extrabold sm:text-3xl md:text-5xl">Stuff We’ve Been Cooking</h1>
+          <p className="font-medium text-white text-sm sm:text-lg md:text-2xl">Check out some of the cool projects we’ve dropped.</p>
         </div>
         <div className="md:grid md:gap-10 md:grid-cols-2 md:mt-10 mt-4
         xl:grid-cols-3
         ">
-        <CardProjects
-          path='src/assets/mnt.jpeg'
-          location='Indondesia, Tanjung Pandan'
-          type='Absence Aplication'
-          title='Ag Conslutant Absences'
-         tools={projectTools.map((tool, index) => (
-            <ToolsProjects
-              key={index} 
-              icons={tool.icons}
-              title={tool.title}
-            />
-          ))}
-          subTitle='lorem ipsum dolor simet  jametr wok wok wiwok 
-          detok adas dadadasdada dasdada dsadasdas dad asd ad a ' />
-        <CardProjects
-          path='src/assets/mnt.jpeg'
-          location='Indondesia, Tanjung Pandan'
-          type='Absence Aplication'
-          title='Ag Conslutant Absences'
-         tools={projectTools.map((tool, index) => (
-            <ToolsProjects
-              key={index} 
-              icons={tool.icons}
-              title={tool.title}
-            />
-          ))}
-          subTitle='lorem ipsum dolor simet  jametr wok wok wiwok 
-          detok adas dadadasdada dasdada dsadasdas dad asd ad a ' />
-        <CardProjects
-          path='src/assets/mnt.jpeg'
-          location='Indondesia, Tanjung Pandan'
-          type='Absence Aplication'
-          title='Ag Conslutant Absences'
-         tools={projectTools.map((tool, index) => (
-            <ToolsProjects
-              key={index} 
-              icons={tool.icons}
-              title={tool.title}
-            />
-          ))}
-          subTitle='lorem ipsum dolor simet  jametr wok wok wiwok 
-          detok adas dadadasdada dasdada dsadasdas dad asd ad a ' />
+          {projects.map((e) => <CardProjects 
+          key={e.id}
+          path={e.img_path}
+          title={e.tittle}
+          location={e.location}
+          tools={projectTools.map((tool, index) => (
+              <ToolsProjects
+                key={index}
+                icons={tool.icons}
+                title={tool.title}
+              />
+            ))}
+          type={e.type}
+          subTitle={e.subtite}
+          /> )}
         </div>
       </div>
     </section>
