7067c03c6aca30fb1c6748aad55ff2bffdd81f4c
dnaz <zibranaby069@gmail.com>
2025-10-11 04:30:00 +0700

    feat(CardsProjectsComponents) fixing cards to the justify-start

--- src/components/ui/CardProjects.tsx
+++ src/components/ui/CardProjects.tsx
@@ -6,10 +6,7 @@ interface Cards {
  subTitle: string,
  type: string,
  location: string,
- tools:React.ReactNode[]
-
-
-}
+ tools:React.ReactNode[]}
 
 export default function CardProjects( cards : Cards) {
     return (
@@ -30,13 +27,13 @@ export default function CardProjects( cards : Cards) {
             duration-200
            hover:opacity-100 scale-80 flex justify-center items-center">{cards.type}</div>
             </div>
-            <div className="px-8 pb-4 flex flex-col gap-4 sm:gap-2 ">
+            <div className="px-8 pb-4 flex flex-col gap-4 sm:gap-2 justify-start items-start">
                 <h1 className="font-bold sm:text-lg ">{cards.title}</h1>
-                <div className="flex flex-row gap-1 text-xs items-center
-                 sm:scale-120 sm:pl-10 md:pl-6 lg:pl-8 xl:pl-6 2xl:pl-10" > <MdLocationPin size={12} className='sm:scale-120'/>{cards.location}</div>
+                <div className="flex flex-row items-center gap-1 
+                text-sm
+                "><MdLocationPin size={15} className=''/>{cards.location}</div>
                 <p className='text-xs font-medium sm:text-base md:text-sm'>{cards.subTitle}</p> 
-                <div className='flex flex-row sm:scale-110 
-                sm:pl-4 md:pl-2 lg:pl-4 xl:pl-2 2xl:pl-5'>
+                <div className='flex flex-row sm:scale-110 justify-start items-end'>
                 {cards.tools.map((tool, i)=> (<div key={i}>{tool}</div>))}
                 </div>
             </div>
