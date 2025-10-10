b2a518830c70b3f22fb62fd06c1cab4770ced5ef
dnaz <zibranaby069@gmail.com>
2025-10-11 04:55:00 +0700

    feat(Projects) adding framer motion in Components

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,6 +3,7 @@ import ToolsProjects from './ui/ToolsProjects'
 import { createClient } from '@supabase/supabase-js'
 import { useState, useEffect } from 'react'
 import { Oval } from 'react-loader-spinner'
+import { easeOut, motion } from 'framer-motion'
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
 type ParamsProjects = {
   id: number,
@@ -31,17 +32,34 @@ export default function Projects() {
   return (
     <section className='w-screen min-h-screen xl:min-h-0 xl:mb-60 p-0 mt-25 md:mt-30 lg:mt-0'>
       <div className="gap-10 sm:px-10 xl:px-0">
-        <div className="p-6 2xl:px-12">
-          <h1 className="text-white text-2xl font-extrabold sm:text-3xl md:text-5xl">What We’ve Been Building</h1>
-          <p className="font-medium text-white text-sm sm:text-lg md:text-2xl">Take a peek at our latest digital experiments and creative builds.</p>
+        <div className="p-6 2xl:px-12"
+        >
+          <motion.h1 className="text-white text-2xl font-extrabold sm:text-3xl md:text-5xl"
+            initial={{ opacity: 0, x: -30 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ amount: 0.2, once: true }}
+            transition={{ duration: 1, ease: "easeOut", delay: 0.5 }}
+
+          >What We’ve Been Building</motion.h1>
+          <motion.p className="font-medium text-white text-sm sm:text-lg md:text-2xl"
+            initial={{ opacity: 0, x: -30 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ amount: 0.2, once: true }}
+            transition={{ duration: 1, ease: "easeOut", delay: 1 }}
+          >Take a peek at our latest digital experiments and creative builds.</motion.p>
         </div>
-          {loading ?
-            <div className="flex justify-center items-center h-60 w-screen ">
-              <Oval color="white" secondaryColor="gray" height={60} />
-            </div>
-            :
-            <div className="md:grid md:gap-10 md:grid-cols-2 md:mt-10 mt-4
-            xl:grid-cols-3 p-4 flex flex-col gap-4 2xl:px-25 2xl:mt-0">{
+        {loading ?
+          <div className="flex justify-center items-center h-60 w-screen ">
+            <Oval color="white" secondaryColor="gray" height={60} />
+          </div>
+          :
+          <motion.div className="md:grid md:gap-10 md:grid-cols-2 md:mt-10 mt-4
+            xl:grid-cols-3 p-4 flex flex-col gap-4 2xl:px-25 2xl:mt-0"
+            initial={{ opacity: 0, scale: 0.98, }}
+            whileInView={{ opacity: 1, scale: 1, }}
+            viewport={{ amount: 0.2, once: true }}
+            transition={{ duration: 1, ease: "easeOut", delay: 1 }}
+          >{
               projects.map((e) =>
               (
                 <CardProjects
@@ -61,8 +79,8 @@ export default function Projects() {
               )
               )
             }
-              </div>
-          }
+          </motion.div>
+        }
       </div>
     </section>
   )
