ee2d1742dcb804c09f7e1df5a8c5662753d65694
dnaz <zibranaby069@gmail.com>
2025-10-11 04:40:00 +0700

    feat(Projects) adding loading condition and looping projecst.tools

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
 import CardProjects from './ui/CardProjects'
 import ToolsProjects from './ui/ToolsProjects'
-import { SiFlutter, SiSupabase, SiFirebase } from 'react-icons/si'
 import { createClient } from '@supabase/supabase-js'
 import { useState, useEffect } from 'react'
 import { Oval } from 'react-loader-spinner'
-import { div } from 'motion/react-client'
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
 type ParamsProjects = {
   id: number,
@@ -19,56 +16,53 @@ type ParamsProjects = {
 }
 export default function Projects() {
   const [projects, setProjects] = useState<ParamsProjects[]>([])
+  const [loading, setLoading] = useState(true);
   async function getProjects() {
     const { data, error } = await supabase.from('servio_projects').select('*');
     if (error) console.log('Terjadi Masalah bro', error)
     else setProjects(data);
+    console.log(data)
+    setLoading(false)
   }
   useEffect(() => {
     console.log('fetching....')
     getProjects();
   }, []);
-
-  const projectTools = [
-    { title: 'Flutter', icons: <SiFlutter size={10} />, },
-    { title: 'Supabase', icons: <SiSupabase size={10} />, },
-    { title: 'Firebase', icons: <SiFlutter size={10} /> },
-    { title: 'OneSignal', icons: <SiFirebase size={10} />, },
-  ];
-  console.log(projects.map((hei) => {
-    console.log(hei.tittle)
-  }))
   return (
     <section className='w-screen min-h-screen xl:min-h-0 xl:mb-60 p-0 mt-25 md:mt-30 lg:mt-0'>
       <div className="gap-10 sm:px-10 xl:px-0">
-        <div className="p-4">
-          <h1 className="text-white text-2xl font-extrabold sm:text-3xl md:text-5xl">Stuff We’ve Been Cooking</h1>
-          <p className="font-medium text-white text-sm sm:text-lg md:text-2xl">Check out some of the cool projects we’ve dropped.</p>
+        <div className="p-6 2xl:px-12">
+          <h1 className="text-white text-2xl font-extrabold sm:text-3xl md:text-5xl">What We’ve Been Building</h1>
+          <p className="font-medium text-white text-sm sm:text-lg md:text-2xl">Take a peek at our latest digital experiments and creative builds.</p>
         </div>
-        <div className="md:grid md:gap-10 md:grid-cols-2 md:mt-10 mt-4
-        xl:grid-cols-3 p-4 flex flex-col gap-4 2xl:px-25">
-          {!projects ?
+          {loading ?
             <div className="flex justify-center items-center h-60 w-screen ">
               <Oval color="white" secondaryColor="gray" height={60} />
             </div>
             :
-            projects.map((e) =>
-              <CardProjects
-                key={e.id}
-                path={e.img_path}
-                title={e.tittle}
-                location={e.location}
-                tools={projectTools.map((tool, index) => (
-                  <ToolsProjects
-                    key={index}
-                    title={tool.title}
-                    icons={tool.icons}
-                  />
-                ))}
-                type={e.type}
-                subTitle={e.subtite}
-              />)}
-        </div>
+            <div className="md:grid md:gap-10 md:grid-cols-2 md:mt-10 mt-4
+            xl:grid-cols-3 p-4 flex flex-col gap-4 2xl:px-25 2xl:mt-0">{
+              projects.map((e) =>
+              (
+                <CardProjects
+                  key={e.id}
+                  path={e.img_path}
+                  title={e.tittle}
+                  location={e.location}
+                  tools={e.tools.map((tools, index) => (
+                    <ToolsProjects
+                      key={index}
+                      title={tools}
+                    />
+                  ))}
+                  type={e.type}
+                  subTitle={e.subtite}
+                />
+              )
+              )
+            }
+              </div>
+          }
       </div>
     </section>
   )
