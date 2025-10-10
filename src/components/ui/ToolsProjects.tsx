5077b8acc82c02fe1069e7ddc3d93530652a8da3
dnaz <zibranaby069@gmail.com>
2025-10-11 04:30:00 +0700

    feat(ToolsProjectsComponents) Removing Icons Params

--- src/components/ui/ToolsProjects.tsx
+++ src/components/ui/ToolsProjects.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 interface tools{
     title: string
-    icons: React.ReactNode
 }
 export default function ToolsProjects(tools : tools) {
     return (
         <div className="bg-gray-800 w-fit p-1 scale-95
          rounded text-xs flex flex-row gap-0.5
-        items-center justify-center ">{tools.icons}
+        items-center justify-center ">
         <span >{tools.title}</span>
         </div>
     )
